5f2d23014a8b1420002b86b3e634dab98a264537
vone <vojtiknemecek@seznam.cz>
2021-10-20 21:06:28 +0200

    Added barcode template project

---
+++ barcodetemplate/app/src/main/java/com/vsb/kru13/barcodetemplate/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vsb.kru13.barcodetemplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
