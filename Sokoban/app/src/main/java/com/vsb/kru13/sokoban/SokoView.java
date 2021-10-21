9bf36df8a3645da02b0d80fe3adbc1772b01c632
voneAbides <vojtiknemecek@seznam.cz>
2021-10-21 10:12:28 +0200

    Soboban

---
+++ Sokoban/app/src/main/java/com/vsb/kru13/sokoban/SokoView.java
@@ -0,0 +1,82 @@
+package com.vsb.kru13.sokoban;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by kru13 on 12.10.16.
+ */
+public class SokoView extends View{
+
+    Bitmap[] bmp;
+
+    int lx = 10;
+    int ly = 10;
+
+    int width;
+    int height;
+
+    private int level[] = {
+            1,1,1,1,1,1,1,1,1,0,
+            1,0,0,0,0,0,0,0,1,0,
+            1,0,2,3,3,2,1,0,1,0,
+            1,0,1,3,2,3,2,0,1,0,
+            1,0,2,3,3,2,4,0,1,0,
+            1,0,1,3,2,3,2,0,1,0,
+            1,0,2,3,3,2,1,0,1,0,
+            1,0,0,0,0,0,0,0,1,0,
+            1,1,1,1,1,1,1,1,1,0,
+            0,0,0,0,0,0,0,0,0,0
+    };
+
+    public SokoView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public SokoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public SokoView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    void init(Context context) {
+        bmp = new Bitmap[6];
+
+        bmp[0] = BitmapFactory.decodeResource(getResources(), R.drawable.empty);
+        bmp[1] = BitmapFactory.decodeResource(getResources(), R.drawable.wall);
+        bmp[2] = BitmapFactory.decodeResource(getResources(), R.drawable.box);
+        bmp[3] = BitmapFactory.decodeResource(getResources(), R.drawable.goal);
+        bmp[4] = BitmapFactory.decodeResource(getResources(), R.drawable.hero);
+        bmp[5] = BitmapFactory.decodeResource(getResources(), R.drawable.boxok);
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        width = w / ly;
+        height = h / lx;
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        for (int i = 0; i < lx; i++) {
+            for (int j = 0; j < ly; j++) {
+                canvas.drawBitmap(bmp[level[i*10 + j]], null,
+                        new Rect(j*width, i*height,(j+1)*width, (i+1)*height), null);
+            }
+        }
+
+    }
+}
